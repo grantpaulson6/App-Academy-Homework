6d2f48e7130c88cf010804b72ba9bd70f3c03dfc
Grant <grantpaulson6@gmail.com>
2019-03-20 07:41:00 -0700

    Complete W2D3

---
+++ W2D3/Rspec/spec/dessert_spec.rb
@@ -0,0 +1,61 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef") }
+  subject(:dessert) { Dessert.new("pizza",10, :chef)}
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq("pizza")
+    end
+
+    it "sets a quantity" do 
+      expect(dessert.quantity).to eq(10)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to be_empty
+    end
+
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{Dessert.new("pizza","10", :chef)}.to raise_error{ArgumentError}
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do 
+      dessert.add_ingredient("cheese")
+      expect(dessert.ingredients).to eq(["cheese"])
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      dessert.add_ingredient("dough")
+      dessert.add_ingredient("cheese")
+      expect(dessert.mix!).to receive(:shuffle!)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+
+    end
+
+    it "raises an error if the amount is greater than the quantity"
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name"
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in"
+  end
+end
